e1b372a2fe40878e0f1d89ed3b1f1bc8da41ec63
MiguelAG24 <ajimluismi@gmail.com>
2024-08-17 22:59:19 -0600

    TraduccionM

---
+++ MANEJO DE CADENAS/MurcielagoJava/src/manejo_cadena/main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package manejo_cadena;
+
+/**
+ *
+ * @author chech
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+           Principal fm = new Principal();
+        fm.show();
+    }
+    
+}
